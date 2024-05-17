899f29cadf1f5d879669d2395b3e325db209b90d
VeraVechy <verachinwa@gmail.com>
2024-05-17 16:55:55 +0100

    updates to services page

--- Desktop/CodeCrafters/script.js
+++ Desktop/CodeCrafters/script.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const quoteSection = document.getElementById("quote");
-  const getQuoteBtn = document.querySelector(".btn-container .btn");
 
-  getQuoteBtn.addEventListener("click", function () {
-    quoteSection.classList.toggle("expanded");
-  });
-});
