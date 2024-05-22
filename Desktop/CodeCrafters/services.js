b46fbb35a90e9a65d597463c212d47496def229d
Okunade Adeola <adeolafunke7@gmail.com>
2024-05-22 08:55:35 +0100

    all field updated

--- Desktop/CodeCrafters/services.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const quoteSection = document.getElementById("quote");
-  const getQuoteBtn = document.querySelector(".btn-container .btn");
-
-  getQuoteBtn.addEventListener("click", function () {
-    quoteSection.classList.toggle("expanded");
-  });
-});
